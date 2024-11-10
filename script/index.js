9d961acc4648b234d0cc44d9901ae3a38075fa16
Sameer Saharan <albaadigoru@gmail.com>
2024-11-10 16:00:13 +0530

    displayPosts function init

--- script/index.js
+++ script/index.js
@@ -23,3 +23,27 @@ async function getUsers() {
     const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
     const data = await response.json();
 }
+
+// https://jsonplaceholder.typicode.com/comments?postId=${1}
+function displayPosts() {
+    postContainer.innerHTML = '';
+    const startIndex = (currPage-1) * postsPerPage;
+    const paginatedPosts = filteredPosts.slice(startIndex, startIndex + postsPerPage);
+
+    paginatedPosts.forEach(post => {
+        const postElement = document.createElement('div');
+        postElement.setAttribute('class', 'post');
+        const postTitle = document.createElement('h3');
+        postTitle.innerHTML = `${post.title}`;
+        const postBody = document.createElement('p');
+        postBody.innerHTML = `${post.body}`;
+        postElement.appendChild(postTitle);
+        postElement.appendChild(postBody);
+        postElement.addEventListener('click', () => {
+            //openPostComments(`${post.id}`)
+        });
+        // Adding post to the Posts Container
+        postContainer.appendChild(postElement);
+    })
+    // 
+}
