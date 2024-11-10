0e2a445db2f7b673c67fd9e54930d406a0548a5a
Sameer Saharan <albaadigoru@gmail.com>
2024-11-10 15:41:13 +0530

    added query selectors

--- script/index.js
+++ script/index.js
@@ -1,1 +1,7 @@
-// https://jsonplaceholder.typicode.com/posts
+const postContainer = document.querySelector('#posts-container');
+const userDropdown = document.querySelector('#user-dropdown');
+const sortDropdown = document.querySelector('#sort-dropdown');
+const searchInput = document.querySelector('#search-input');
+const prevPageBtn = document.querySelector('#prevPage');
+const nextPageBtn = document.querySelector('#nextPage')
+const currPage = document.querySelector('#currPage');
