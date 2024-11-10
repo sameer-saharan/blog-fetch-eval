efdfaf74f64cc9d1070bbc626ad2351667fb1c79
Sameer Saharan <albaadigoru@gmail.com>
2024-11-10 15:45:54 +0530

    fetch users and posts

--- script/index.js
+++ script/index.js
@@ -4,4 +4,22 @@ const sortDropdown = document.querySelector('#sort-dropdown');
 const searchInput = document.querySelector('#search-input');
 const prevPageBtn = document.querySelector('#prevPage');
 const nextPageBtn = document.querySelector('#nextPage')
-const currPage = document.querySelector('#currPage');
+const currPageSpan = document.querySelector('#currPage');
+
+let posts = [];
+let users = [];
+let filteredPosts = [];
+let currPage = 1;
+const postsPerPage = 10;
+
+// Fetch Posts and Users
+async function getPosts() {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+    const data = await response.json();
+    filteredPosts = data;
+}
+
+async function getUsers() {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+    const data = await response.json();
+}
